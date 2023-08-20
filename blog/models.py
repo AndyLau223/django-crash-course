9d845f485dda7f0d4ccc5f692dd0c17a627650ff
Andy <jiancong.liu@outlook.com>
2023-08-20 14:43:24 +0800

    add descriptive columns for post in admin

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,11 @@ STATUS = ((0, 'Draft'), (1, 'Publish'))
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    data_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, unique=True)
     # if user is deleted from User table, then its blog will be deleted as well.
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
