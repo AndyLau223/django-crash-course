586cdc8ba6dc159ebd933c01aeb83b553e980b1c
Andy <jiancong.liu@outlook.com>
2023-08-20 14:50:27 +0800

    add static home page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 # urls.py connects to views for incoming requests
 
 urlpatterns = [
-    path('<slug:slug>', views.BlogView.as_view(), name='blog_view')
+    path('<slug:slug>', views.BlogView.as_view(), name='blog_view'),
+    path('', views.HomeView.as_view(), name='home_view')
 ]
